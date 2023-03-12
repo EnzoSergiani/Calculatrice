b3939120f9556a25c5a320af48150cfbe6fb86da
Enzo Sergiani <enzo.sergiani@student.junia.com>
2023-03-12 10:18:29 +0100

    Add images

---
+++ images/README.md
@@ -0,0 +1,1 @@
+Icons by Freepik PixelPerferct
---
+++ scripts/index.js
@@ -0,0 +1,131 @@
+// TODO : Enlever le "undefined"
+
+//#region --- Outils ---
+function dechargePile(values) {
+  for (let i = 0; i < pile.length; i++) {
+    console.log("> ",pile[i]);
+    values += pile[i];
+  }
+  return values;
+}
+
+//#endregion
+
+//#region --- Opérations ---
+function calcul(pile){
+  try {
+    answer = eval(pile.join(""));
+    console.log(answer);
+    pile = [answer]
+    return answer;
+  } catch (erreur) {
+    printError();
+    return;
+  }
+}
+//#endregion
+
+//#region --- Affichage ---
+function printResult(values) {
+  document.getElementById("answer").innerHTML = values;
+}
+
+function printHistoric(answer){
+  document.getElementById("historic").innerHTML = answer;
+}
+
+function printError(){
+  document.getElementById("answer").innerHTML = "Error syntax"
+}
+
+function clearResult() {
+  document.getElementById("answer").innerHTML = "";
+ 
+}
+
+function clearHistoric(){
+  document.getElementById("historic").innerHTML = "";
+}
+
+//#endregion
+
+const buttons = document.querySelectorAll("button");
+const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var pile = [];
+let values;
+let answer;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const id = event.target.id;
+    console.log("Pile :",pile);
+    console.log("Bouton :",id);
+    if (id == "clear"){
+      clearResult();
+      clearHistoric();
+      pile = [];
+    }
+    else if (id == "delete"){
+      pile.pop();
+      printResult(dechargePile());
+    }
+    else if (id == "egal") {
+      printHistoric(dechargePile())
+      clearResult();
+      answer = calcul(pile)
+      pile = [answer.toString()]
+      printResult(answer);
+    }
+    else if (id == "addition") {
+      pile.push("+");
+      printResult(dechargePile());
+    }
+    else if (id == "soustraction") {
+      pile.push("-");
+      printResult(dechargePile());
+    }
+    else if (id == "multiplication") {
+      pile.push("*");
+      printResult(dechargePile());
+    }
+    else if (id == "division") {
+      pile.push("/");
+      printResult(dechargePile());
+    }
+    else if (id == "mod") {
+      pile.push("%");
+      printResult(dechargePile());
+    }
+    else if (id == "puissance") {
+      pile.push("^");
+      printResult(dechargePile());
+    }
+    else if (id == "pi") {
+      pile.push("(3.14)");
+      printResult(dechargePile());
+    }
+    else if (id == "parentheseOuv") {
+      pile.push("(");
+      printResult(dechargePile());
+    }
+    else if (id == "parentheseFer") {
+      pile.push(")");
+      printResult(dechargePile());
+    }
+    else if (id == "racine") {
+      pile.push(" sqrt(");
+      printResult(dechargePile());
+    }
+    else if (id == "point") {
+      pile.push(".");
+      printResult(dechargePile());
+    }
+    else if (id in number) {
+      pile.push(id);
+      console.log("> Pile :",pile);
+      printResult(dechargePile());
+    } else {
+      console.error("Erreur input");
+    }
+  });
+});
